8b2979ad398f74dedd48a4d79be60ae4e3816de8
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-05-20 21:26:25 +0300

    input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, useState } from "react";
+import { api } from "../utils/api";
+
+
+
+const Input = () => {
+
+  const [selectedFile, setSelectedFile] = useState<File>();
+  const [inputText, setInputText] = useState('')
+  const handleSetFile = (e:  ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setSelectedFile(e.target.files[0])
+    }
+  }
+
+  const handleCreatePost = async (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    if (selectedFile) {
+      formData.append('filedata', selectedFile)
+    }
+    formData.append('body', inputText)
+    api.createPost(formData)
+  }
+
+  return (
+    // <form className="my-5" encType="multipart/form-data"  method="post" action="http://localhost:3000/post">
+    <form className="my-5" encType="multipart/form-data"  onSubmit={handleCreatePost}>
+      <div>
+      <textarea className="w-[100%] resize scroll-hidden" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+      <div className="">
+        <input type="file" accept="image/png, image/jpeg" name="filedata" onChange={handleSetFile} />
+      </div>
+      </div>
+      <button type="submit">POST</button>
+    </form>
+  );
+};
+
+export default Input;
