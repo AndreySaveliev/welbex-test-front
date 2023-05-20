8446aafb6074dcc48c456d86703e04c9f9d8ffbc
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-05-20 21:25:51 +0300

    api class

---
+++ src/utils/api.ts
@@ -0,0 +1,58 @@
+class Api {
+  baseUrl: string
+  constructor() {
+    this.baseUrl = 'http://localhost:3000'
+  }
+
+
+  async login(email: string, password: string) {
+    try {
+      const res = await fetch(`${this.baseUrl}/signin`, { credentials: 'include', method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ email, password }) })
+      return res.json()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async register(email: string, name: string, password: string) {
+    try {
+      const res = await fetch(`${this.baseUrl}/signup`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ email, password, name }) })
+      return res
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async getAllPosts() {
+    try {
+      const res = await fetch(`${this.baseUrl}/post`, { credentials: 'include' })
+      return res.json()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async checkMe(token) {
+    try {
+      const res = await fetch(`${this.baseUrl}/user`, { credentials: 'include', headers: { 'Content-Type': 'application/json', 'Authorization': `Welbex ${token}` } })
+      return res.json()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async createPost(formdata) {
+    try {
+      const res = await fetch(`${this.baseUrl}/post/`, {
+        method: "POST",
+        credentials: 'include',
+        body: formdata
+      })
+      return res.json
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+export const api = new Api()
