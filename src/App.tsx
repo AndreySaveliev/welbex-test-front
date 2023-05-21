83cdc071b9c715ff42b63e49ff3b2bc8e0522b0c
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-05-21 17:21:26 +0300

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
-import { useEffect, useState } from 'react';
 import ProtectedRoutes from './ProtectedRoutes'
-import { api } from './utils/api';
 
 function App() {
-  const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'))
-  const navigate = useNavigate();
-  
-  // useEffect(() => {
-  //   if (localStorage.getItem('token')) {
-  //     setIsLoggedIn(true)
-  //     navigate('/')
-  //   }
-  // }, [navigate])
-
-  // useEffect(() => {
-  //   if (localStorage.getItem('token')) {
-  //     const token = JSON.parse(localStorage.getItem('token'))
-  //     api.checkMe(token).then((res) => console.log(res))
-  //   }
-  // },[])
+  const isLoggedIn = !!JSON.stringify(localStorage.getItem('isLoggedIn'))
 
   return (
     <Routes>
