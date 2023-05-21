77d2d9f31c48e09e31c30e5d7ae15867b54e287b
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-05-21 17:21:04 +0300

    post container html

--- src/components/PostContaine.tsx
+++ src/components/PostContaine.tsx
@@ -1,51 +1,80 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useCallback, useContext } from 'react';
 import { Post } from '../types/types';
 import moment from 'moment';
 import Popup from './Popup';
-
+import { MdEdit, MdOutlineDelete } from 'react-icons/md';
+import { api } from '../utils/api';
+import { UserContext } from '../context/userContex';
+import EditPopup from './EditPopup';
 interface PostContainerProps {
   post: Post;
 }
 
 const PostContainer: React.FC<PostContainerProps> = ({ post }) => {
   const [isShown, setIsShown] = useState(false);
+  const [isEditPopupShown, setIsEditPopupShown] = useState(false);
+  const user = useContext(UserContext);
+  const handleDeletePost = () => {
+    api.deletePost(post.id);
+  };
+
+  const handleToggleEditPopup = useCallback(() => {
+    if (isEditPopupShown) {
+      setIsEditPopupShown(false);
+    } else {
+      setIsEditPopupShown(true);
+    }
+  }, [isEditPopupShown]);
 
   const handleTogglePopup = useCallback(() => {
     if (isShown) {
-      setIsShown(false)
+      setIsShown(false);
     } else {
-      setIsShown(true)
+      setIsShown(true);
     }
-  }, [isShown])
+  }, [isShown]);
 
   return (
-    <>
-      <div
-        className="w-[60%] flex flex-row h-20 bg-yellow-500 mb-3 cursor-pointer px-2"
-        onClick={() => setIsShown(true)}
-      >
-        {post.media && (
-          <img
-            src={`http://localhost:3000/uploadfile/${post.media}`}
-            className="overflow-y-hidden object-contain object-center w-[30%]"
-          ></img>
-        )}
-        <div className="w-auto flex items-center flex-1">
-          <p>{post.body}</p>
-        </div>
-        <div className="w-auto flex flex-col items-center flex-1 justify-end">
-          <p>{post.authorId}</p>
-          <p>{moment(post.createdAt).format('L, LT')}</p>
+    <div className="mx-auto w-[80%]">
+      <div className="flex h-20 bg-yellow-500 mb-3 px-2">
+        <div className="flex flex-1 flex-row overflow-hidden" >
+          {post.media && (
+            <img
+              src={`http://localhost:3000/uploadfile/${post.media}`}
+              className="overflow-y-hidden object-contain object-center w-[30%] cursor-pointer hover:scale-150  transition duration-500"
+              onClick={() => setIsShown(true)}
+            ></img>
+          )}
+          <div className="w-auto flex flex-1 overflow-hidden items-center text-ellipsis">
+            <p className="truncate  cursor-pointer hover:font-semibold" onClick={() => setIsShown(true)}>{post.body}</p>
+          </div>
+          {post.authorId === user.id && (
+            <>
+              <MdEdit
+                size={30}
+                className="self-center cursor-pointer mr-3 hover:opacity-60 transition duration-300"
+                onClick={handleToggleEditPopup}
+              />
+              <MdOutlineDelete
+                size={30}
+                className="self-center cursor-pointer hover:opacity-60 transition duration-300"
+                onClick={handleDeletePost}
+              />
+            </>
+          )}
+          <div className="w-fit flex flex-col items-center justify-center pointer-events-none">
+            <p>{post.authorId}</p>
+            <p>{moment(post.createdAt).format('L, LT')}</p>
+          </div>
         </div>
       </div>
-      <div
-        className={`absolute w-full h-full bg-black/40 cursor-pointer ${
-          isShown ? 'block' : 'hidden'
-        }`}
-      >
-        <Popup post={post} isShown={isShown} handleTogglePopup={handleTogglePopup}/>
-      </div>
-    </>
+      <EditPopup
+        isEditPopupShown={isEditPopupShown}
+        handleToggleEditPopup={handleToggleEditPopup}
+        post={post}
+      />
+      <Popup post={post} isShown={isShown} handleTogglePopup={handleTogglePopup} />
+    </div>
   );
 };
 
